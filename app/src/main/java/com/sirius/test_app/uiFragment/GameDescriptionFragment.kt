3ee33e5ca7bf5306f4d1f77fa7119ccebc95259e
Murad Ahmedov <code.exe.machine1337@gmail.com>
2022-07-13 17:43:45 +0300

    fix game description layout

--- app/src/main/java/com/sirius/test_app/uiFragment/GameDescriptionFragment.kt
+++ app/src/main/java/com/sirius/test_app/uiFragment/GameDescriptionFragment.kt
@@ -5,10 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.sirius.test_app.data.DataModel
 import com.sirius.test_app.databinding.FragmentGameDescriptionBinding
 
 class GameDescriptionFragment : Fragment() {
+
     private lateinit var binding: FragmentGameDescriptionBinding
+    private val data = DataModel()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -16,6 +19,14 @@ class GameDescriptionFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentGameDescriptionBinding.inflate(layoutInflater, container, false)
+
+        binding.tvGameName.text = data.name
+        binding.tvReviewCnt.text = data.gradeCnt
+        binding.tvGameDescription.text = data.description
+        binding.tvGameRating.text = data.rating.toString()
+
+
         return binding.root
     }
+
 }
